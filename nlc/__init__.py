93d2ab3cf8ea729dc2565a9e7896749785a55210
alex4321 <gaussmake1994@yandex.ru>
2016-07-16 00:19:40 +0300

    Deleted old code

--- nlc/__init__.py
+++
@@ -1,5 +0,0 @@
-from .word2vec import Word2Vec, Vector
-from .extractor import Extractor
-from .word2vec_test import Word2VecTest
-from .classifier import Classes, Classifier
-from .classifier_test import ClassifierTest
